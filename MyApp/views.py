4b9a4d4f8a5f594adad9fb42a28fbfab155deafa
wangzijia <wangzijia@xiaozhu.com>
2021-01-12 09:54:07 +0800

    .

--- MyApp/views.py
+++ MyApp/views.py
@@ -738,3 +738,95 @@ def project_login_save(request):
     # 返回
     return HttpResponse('success')
 
+# 调试登陆态接口
+def project_login_send(request):
+    # 第一步，获取前端数据
+    login_method = request.GET['login_method']
+    login_url = request.GET['login_url']
+    login_host = request.GET['login_host']
+    login_header = request.GET['login_header']
+    login_body_method = request.GET['login_body_method']
+    login_api_body = request.GET['login_api_body']
+    login_response_set = request.GET['login_response_set']
+    # 第二步，发送请求
+    try:
+        header = json.loads(login_header) #处理header
+    except:
+        return HttpResponse('请求头不符合json格式！')
+
+    # 拼接完整url
+    if login_host[-1] == '/' and login_url[0] =='/': #都有/
+        url = login_host[:-1] + login_url
+    elif login_host[-1] != '/' and login_url[0] !='/': #都没有/
+        url = login_host+ '/' + login_url
+    else: #肯定有一个有/
+        url = login_host + login_url
+    try:
+        if login_body_method == 'none':
+            response = requests.request(login_method.upper(), url, headers=header, data={} )
+        elif login_body_method == 'form-data':
+            files = []
+            payload = {}
+            for i in eval(login_api_body):
+                payload[i[0]] = i[1]
+            response = requests.request(login_method.upper(), url, headers=header, data=payload, files=files )
+
+        elif login_body_method == 'x-www-form-urlencoded':
+            header['Content-Type'] = 'application/x-www-form-urlencoded'
+            payload = {}
+            for i in eval(login_api_body):
+                payload[i[0]] = i[1]
+            response = requests.request(login_method.upper(), url, headers=header, data=payload )
+
+        elif login_body_method == 'GraphQL':
+            header['Content-Type'] = 'application/json'
+            query = login_api_body.split('*WQRF*')[0]
+            graphql = login_api_body.split('*WQRF*')[1]
+            try:
+                eval(graphql)
+            except:
+                graphql = '{}'
+            payload = '{"query":"%s","variables":%s}' % (query, graphql)
+            response = requests.request(login_method.upper(), url, headers=header, data=payload )
+
+
+        else: #这时肯定是raw的五个子选项：
+            if login_body_method == 'Text':
+                header['Content-Type'] = 'text/plain'
+
+            if login_body_method == 'JavaScript':
+                header['Content-Type'] = 'text/plain'
+
+            if login_body_method == 'Json':
+                header['Content-Type'] = 'text/plain'
+
+            if login_body_method == 'Html':
+                header['Content-Type'] = 'text/plain'
+
+            if login_body_method == 'Xml':
+                header['Content-Type'] = 'text/plain'
+            response = requests.request(login_method.upper(), url, headers=header, data=login_api_body.encode('utf-8'))
+        # 把返回值传递给前端页面
+        response.encoding = "utf-8"
+        DB_host.objects.update_or_create(host=login_host)
+        res = response.json()
+        # 第三步，对返回值进行提取
+        get_res = '' #声明提取结果存放
+        for i in login_response_set.split('\n'):
+            if i == "":
+                continue
+            else:
+                i = i.replace(' ','')
+                key = i.split('=')[0] #拿出key
+                path = i.split('=')[1]  #拿出路径
+                value = res
+                for j in path.split('/')[1:]:
+                    value = value[j]
+                get_res += key + '="' + value +'"\n'
+        end_res = {"response":response.text,"get_res":get_res}
+        return HttpResponse(json.dumps(end_res),content_type='application/json')
+
+    except Exception as e:
+        end_res = {"response":str(e),"get_res":''}
+        return HttpResponse(json.dumps(end_res),content_type='application/json')
+
--- MyApp/views_tools.py
+++ MyApp/views_tools.py
@@ -6,7 +6,11 @@ from django.shortcuts import render
 from MyApp.models import *
 import json
 import requests
-
+from allpairspy import AllPairs
+import xlrd
+import xlwt
+from xlutils import copy
+import os
 
 # 获取公共参数
 def glodict(request):
@@ -18,3 +22,34 @@ def glodict(request):
 # 进入正交工具页面
 def zhengjiao(request):
     return render(request,'welcome.html',{"whichHTML": "zhengjiao.html","oid":request.user.id,**glodict(request) })
+
+# 正交工具运行
+def zhengjiao_play(request):
+    end_values = request.GET['end_values'].split(',')
+    new_values = [ i.split('/') for i in end_values ]
+    res = []
+    for i in AllPairs(new_values):
+        res.append(i)
+    d = { "res" : res}
+    return HttpResponse(json.dumps(d),content_type="application/json")
+
+# 正交工具导出
+def zhengjiao_excel(request):
+    end_keys = request.GET['end_keys'].split(',')
+    end_values = request.GET['end_values'].split(',')
+    new_values = [i.split('/') for i in end_values]
+    res = []
+    for i in AllPairs(new_values):
+        res.append(i)
+    # 先创建
+    wqrf_book = xlwt.Workbook(encoding='utf-8') # 创建excel
+    wqrf_sheet = wqrf_book.add_sheet("正交结果") # 创建sheet页
+    for i in range(len(res)):
+        case_index = '用例:'+str(i+1) # 用例序号
+        hb = list(zip(end_keys,res[i])) #把key和value进行合并
+        case = ','.join([':'.join(list(i)) for i in hb]) #进行格式化，便于阅读
+        wqrf_sheet.write(i,0,case_index)  # 写入，i为行，0为第一例
+        wqrf_sheet.write(i, 1, case)  # 写入，i为行，1为第二例
+    wqrf_book.save('MyApp/static/tmp_zhengjiao.xls') #保存
+
+    return HttpResponse('')
